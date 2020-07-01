d894044db69ae7c3ff22af6cf96157d33d835896
vishalrranjan <vishalkr275@gmail.com>
2020-07-01 15:36:48 +0530

    added the required file for host

---
+++ build/precache-manifest.ce506a91893f3dff1041c98046ee4644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6db9b6d901d0c6d03d450c4f068de9",
+    "url": "/nutrition-app/index.html"
+  },
+  {
+    "revision": "8dd711d155de809ff0b1",
+    "url": "/nutrition-app/static/css/main.e0bc124d.chunk.css"
+  },
+  {
+    "revision": "d08f36887e1c34b826ae",
+    "url": "/nutrition-app/static/js/2.47a85cc6.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/nutrition-app/static/js/2.47a85cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd711d155de809ff0b1",
+    "url": "/nutrition-app/static/js/main.5080ee9f.chunk.js"
+  },
+  {
+    "revision": "5919892f8ff2a6198fce",
+    "url": "/nutrition-app/static/js/runtime-main.dec82f62.js"
+  }
+]);
